b0e7e068e0c1dcf0c234433d4df26f9671c82512
Angelica Diez <angelica.diez08@gmail.com>
2020-06-04 10:49:36 +0200

    added password encoder

--- src/main/java/com/example/config/CustomWebSecurityConfigurerAdapter.java
+++ src/main/java/com/example/config/CustomWebSecurityConfigurerAdapter.java
@@ -3,12 +3,15 @@ package com.example.config;
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -24,9 +27,10 @@ public class CustomWebSecurityConfigurerAdapter extends WebSecurityConfigurerAda
 	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
 		auth.jdbcAuthentication()
 			.dataSource(dataSource)
-			.usersByUsernameQuery("SELECT username, password, true FROM users WHERE username = ?")
+			.usersByUsernameQuery("SELECT username, password, 'true' as enabled FROM users WHERE username = ?")
 			.authoritiesByUsernameQuery("SELECT u.username, r.authority FROM roles r, users u "
-										+ "WHERE u.username = ? AND u.id = r.id_username");
+										+ "WHERE u.username = ? AND u.id = r.id_username")
+			.passwordEncoder(passwordEncoder());
 		
 //		auth.jdbcAuthentication().dataSource(dataSource).and().userDetailsService(myUserDetailsService);
 	}
@@ -41,9 +45,11 @@ public class CustomWebSecurityConfigurerAdapter extends WebSecurityConfigurerAda
 //			.anyRequest().authenticated();
 	}
 	
-//	@Bean
-//	public PasswordEncoder passwordEncoder() {
-//	    return new BCryptPasswordEncoder();
-//	}
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+//		String encoded = new BCryptPasswordEncoder().encode(plainText);
+//		System.out.println(encoded);
+	    return new BCryptPasswordEncoder();
+	}
 
 }
